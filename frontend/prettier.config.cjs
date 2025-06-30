c89e735b945f870c308bcdb86a541900002abba9
ViniciusVon <vinnyfut10@gmail.com>
2025-06-29 22:01:35 -0300

    Adicionando componente de carrossel, card, nav e mudança de tema

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,7 @@
+// prettier.config.js ou .prettierrc.js
+module.exports = {
+  semi: true, // ou false, se não quiser ponto e vírgula
+  singleQuote: true, // ou false, se quiser aspas duplas
+  trailingComma: 'all',
+  tabWidth: 2,
+};
